a12750849838b6d06aaa5c3e9582ab6a3973482f
Grigorich <oskinnick@gmail.com>
2024-09-21 14:30:13 +0300

    Initial empty

---
+++ src/main/java/org/mykola/HebrewStudy/HebrewStudyApplication.java
@@ -0,0 +1,13 @@
+package org.mykola.HebrewStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HebrewStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HebrewStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mykola/HebrewStudy/HebrewStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mykola.HebrewStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HebrewStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
